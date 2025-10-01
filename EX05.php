279dab79340a0bbe8bef572c1740bd5aebde5109
Adriano Canto Junior <adrianocanto462@gmail.com>
2025-10-01 17:11:21 -0300

    EX05

---
+++ EX05.php
@@ -0,0 +1,22 @@
+<?php
+
+    $nome = "Adriano Canto Junior";
+    $profissao = "Estágiario de TI do IFSC Campus Garopaba";
+    $cidade = "Imbituba";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cartão de Apresentação</title>
+</head>
+<body>
+    <h1>Cartão de Apresentação</h1>
+    <?=$nome?><br>
+    <?=$profissao?><br>
+    <?=$cidade?>
+</body>
+</html>
