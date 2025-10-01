5d112812a4ba26f6e29257c226674392500f9f35
Adriano Canto Junior <adrianocanto462@gmail.com>
2025-10-01 16:53:11 -0300

    EX03

---
+++ EX03.php
@@ -0,0 +1,10 @@
+<?php
+
+    $produto = 5;
+    $desconto = 0.80;
+    $preco_desconto = $produto * $desconto;
+
+    echo "O preço original do produto éra de R$ $produto,00 e com desconto de 10% fica R$ $preco_desconto,00"
+
+
+?>
