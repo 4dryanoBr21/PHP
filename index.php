07c3edb5eda9ba16c848e07f4ac97326f41c7004
Adriano Canto Junior <adrianocanto462@gmail.com>
2025-10-01 16:13:33 -0300

    Primeiro EX

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-    echo "Ola Mundo!";
+    $nome = "Adriano";
+    $idade = 19;
+    $cidade = "Imbituba";
+
+    echo "Olá meu nome é ",$nome," tenho ",$idade," anos e moro em ",$cidade;
+
 ?>
